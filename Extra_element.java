db89a9b81fd23fb27212bf9802607df1b5cb4773
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-04-24 06:06:40 +0530

    Update Extra_element.java

--- Extra_element.java
+++ Extra_element.java
@@ -1,24 +1,22 @@
 //Find the extra element's index
-public class Extra_element {
-        
+public class ExtraElement {
 
-        static int findExtra(int arr1[], int arr2[], int n) {
-            for (int i = 0; i < n; i++)
-                if (arr1[i] != arr2[i])
-                    return i;
-    
-           
-            return n;
+    static int findExtra(int arr1[], int arr2[], int n) {
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != arr2[i]) {
+                return i;
+            }
         }
-  
-        public static void main(String[] args) {
-            int arr1[] = {2, 4, 6, 8, 10, 12, 13};
-            int arr2[] = {2, 4, 6, 8, 10, 12};
-            int n = arr2.length;
-    
-            
-            System.out.println(findExtra(arr1, arr2, n));
-        }
+        return n;
     }
+
+    public static void main(String[] args) {
+        int arr1[] = {2, 4, 6, 8, 10, 12, 13};
+        int arr2[] = {2, 4, 6, 8, 10, 12};
+        int n = arr2.length;
+        System.out.println(findExtra(arr1, arr2, n));
+    }
+}
+
     
 
