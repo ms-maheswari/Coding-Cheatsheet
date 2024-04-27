9b4a6195aac8973357a5e679b1b303f0312ce7db
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-04-27 05:53:25 +0530

    first commit

--- Patterns/Rectangular.java
+++ Patterns/Rectangular.java
@@ -1,24 +1,19 @@
-// Pattern-1: Rectangular Star Pattern
+//  Rectangular Star Pattern
 class Rectangular {
-static void pattern1(int N)
- {
-     for (int i = 0; i < N; i++)
-     {
-         for (int j = 0; j < N; j++)
-         {
-             System.out.print("* ");
-         }
-         // for next row, give a line break
-         System.out.println();
-     }
- }
- 
-     public static void main(String[] args) {
+    static void printPattern(int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 
-         int N = 6;
-         pattern1(N);
-     }
- }
+    public static void main(String[] args) {
+        int N = 5;
+        printPattern(N);
+    }
+}
 
 // Output:
 
