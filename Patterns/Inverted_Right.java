f2584e9c85dd1fb177024428702ffc2ce66ba0f0
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-04-27 06:10:59 +0530

    first commit

---
+++ Patterns/Inverted_Right.java
@@ -0,0 +1,26 @@
+// Inverted Right Pyramid
+public class pattern5 {
+    static void Inverted_Right(int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j > i; j++) { 
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 6;
+        printPattern(N);
+    }
+}
+
+
+// Output:
+
+// * * * * * *
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
