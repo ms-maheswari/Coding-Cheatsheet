c3d0309be3c61caf28f45e06035b55ebe63089d5
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-04-28 14:29:50 +0530

    Create Star_Pyramid.java

---
+++ Patterns/Star_Pyramid.java
@@ -0,0 +1,28 @@
+// Star Pyramid
+public class Star_Pyramid {
+    static void printpattern(int n){
+        for(int i = 1;i<=n;i++){
+            for(int j=i;j<=n;j++){       //Decreasing space
+System.out.print("  ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print("* ");  //Increasing star
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");  //Increasing star
+            }
+            System.out.println();  
+        }
+    }
+    public static void main(String[] args){
+        int n = 5;
+        printpattern(n);
+} 
+}
+
+
+  //         * 
+  //       * * *
+  //     * * * * *
+  //   * * * * * * *
+  // * * * * * * * * *
