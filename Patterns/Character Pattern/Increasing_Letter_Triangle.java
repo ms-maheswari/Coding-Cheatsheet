cb47482ddd716c6cb6c2b49bb9903dc50791c4f6
ms-maheswari <mahes7439@gmail.com>
2024-04-30 20:32:26 +0530

    first commit

---
+++ Patterns/Character Pattern/Increasing_Letter_Triangle.java
@@ -0,0 +1,18 @@
+public class Increasing_Letter_Triangle {
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern(n);
+    }
+
+    private static void printPattern(int n) {
+       
+
+        for(int i=1;i<=n;i++){
+            for(char ch = 'A'; ch<='A'+i;ch++){
+                System.out.print(ch + " ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
