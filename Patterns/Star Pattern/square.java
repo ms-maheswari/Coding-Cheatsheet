57d8842741760d3a04ac648a41d58edd07cf8b38
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-05-02 04:40:28 +0530

    first commit

--- Patterns/Star Pattern/square.java
+++ Patterns/Star Pattern/square.java
@@ -1,25 +1,24 @@
 //  Rectangular Star Pattern
-class Rectangular {
+class square {
     static void printPattern(int N) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print("* ");
+                System.out.print("*  ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int N = 5;
+        int N = 3;
         printPattern(N);
     }
 }
 
 // Output:
 
-// * * * * * *
-// * * * * * *
-// * * * * * *
-// * * * * * *
-// * * * * * *
-// * * * * * *
+// *  *  * 
+// *  *  * 
+// *  *  * 
+
+
